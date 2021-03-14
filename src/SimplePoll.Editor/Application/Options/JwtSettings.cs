653b8edc5f7716477266148fd20e6c2ee713fa3e
Insaf Ismagilov <insaf.ismg@gmail.com>
2021-03-14 16:05:25 +0300

    Add jwt settings.

---
+++ src/SimplePoll.Editor/Application/Options/JwtSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace SimplePoll.Editor.Application.Options
+{
+	public class JwtSettings
+	{
+		public string IdentityUrl { get; set; }
+		public string Audience { get; set; }
+	}
+}
---
+++ src/SimplePoll.Editor/Configurations/AuthConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using SimplePoll.Editor.Application.Options;
+
+namespace SimplePoll.Editor.Configurations
+{
+	internal static class AuthConfiguration
+	{
+		internal static IServiceCollection ConfigureAuth(this IServiceCollection services, IConfiguration configuration)
+		{
+			var jwtSettingsSection = configuration.GetSection(nameof(JwtSettings));
+			services.Configure<JwtSettings>(jwtSettingsSection);
+
+			services
+				.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+				})
+				.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+				{
+					options.SaveToken = true;
+					options.Authority = jwtSettingsSection[nameof(JwtSettings.IdentityUrl)];
+					options.RequireHttpsMetadata = false;
+					options.Audience = jwtSettingsSection[nameof(JwtSettings.Audience)];
+
+					options.Events = new JwtBearerEvents
+					{
+						OnForbidden = c =>
+						{
+							var email = c.HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value;
+							var logger = services.BuildServiceProvider().GetService<ILogger<Startup>>();
+							logger.LogError("Access denied for {Email}. Path: {Path}", email, c.HttpContext.Request.Path);
+							return Task.CompletedTask;
+						}
+					};
+				});;
+			
+			return services;
+		}
+	}
+}
