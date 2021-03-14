c4ff77483aa13efa30643c08b2770766e75faa27
Insaf Ismagilov <insaf.ismg@gmail.com>
2021-03-14 15:44:04 +0300

    Add Identity Authentication.

--- src/SimplePoll.Identity/Configurations/AuthConfiguration.cs
+++ src/SimplePoll.Identity/Configurations/AuthConfiguration.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using SimplePoll.Identity.Application.Options;
 
 namespace SimplePoll.Identity.Configurations
@@ -10,6 +17,38 @@ namespace SimplePoll.Identity.Configurations
 		{
 			var jwtSettingsSection = configuration.GetSection(nameof(JwtSettings));
 			services.Configure<JwtSettings>(jwtSettingsSection);
+
+			services
+				.AddAuthentication(options =>
+				{
+					options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+					options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+				})
+				.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+				{
+					options.SaveToken = true;
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						ValidIssuer = jwtSettingsSection[nameof(JwtSettings.Issuer)],
+						ValidAudience = jwtSettingsSection[nameof(JwtSettings.Audience)],
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettingsSection[nameof(JwtSettings.SigningKey)]))
+					};
+
+					options.Events = new JwtBearerEvents
+					{
+						OnForbidden = c =>
+						{
+							var email = c.HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Email)?.Value;
+							var logger = services.BuildServiceProvider().GetService<ILogger<Startup>>();
+							logger.LogError("Access denied for {Email}. Path: {Path}", email, c.HttpContext.Request.Path);
+							return Task.CompletedTask;
+						}
+					};
+				});;
 			
 			return services;
 		}
